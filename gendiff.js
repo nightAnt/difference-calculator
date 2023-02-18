1b4a677816bc62b6c4e64b9de12bccd1115bdb0c
nightAnt <nikolayantoshkin2000@gmail.com>
2023-02-18 22:45:42 +0300

    update gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -7,6 +7,6 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1>')
-  .argument('<filepath2>');
-program.parse();
+  .arguments('<filepath1> <filepath2>');
+
+  program.parse();
