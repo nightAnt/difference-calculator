8d55fa3714148e8f87ff99a8bf2b781838c6ac42
nightAnt <nikolayantoshkin2000@gmail.com>
2023-02-19 00:55:36 +0300

    update

--- gendiff.js
+++ gendiff.js
@@ -1,12 +1,63 @@
 #!/usr/bin/env node
-
+import { resolve } from 'path';
 import { Command }  from 'commander';
+import { readFile, readFileSync } from 'fs';
+import _ from 'lodash';
+
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action( (filePath1, filePath2) => {  
+    let ob1 = readFileSync(resolve(filePath1));
+    let ob2 = readFileSync(resolve(filePath2)); 
+    
 
-  program.parse();
+  });
+
+program.parse();
+/*
+let [file1, file2] = program.args;
+let filePath1 = resolve(file1);
+let ob1 = readFileSync(filePath1);
+let ob2 = readFileSync('file2');
+
+console.log(JSON.parse(ob1));
+
+
+const stringify = (data, replacer = ' ', spacesCount = 1) => { 
+  const iter = (innerData, depth) => { 
+    if (!_.isObject(innerData)) { 
+      return `${innerData}`; 
+    } 
+    const entries = Object.entries(innerData); 
+    const result = entries.map(([key, value]) => { 
+      const startIndent = replacer.repeat(depth * spacesCount); 
+      return `${startIndent}${key}: ${iter(value, depth + 1)}`; 
+    }); 
+    const endIndent = replacer.repeat((depth - 1) * spacesCount); 
+    const out = ['{', ...result, `${endIndent}}`].join('\n'); 
+    return out; 
+  }; 
+  return iter(data, 1); 
+}; 
+
+
+let ress = [];
+for (let [key, value] in Object.entries(ob1)) {
+  if (key in ob2) {
+    if (value === ob2[key]) {
+      ress.push(['', key, value]);
+    } else {
+      ress.push(['-', key, value]); 
+      ress.push(['+', key, value]);
+    };
+  };
+};
+
+
+
+*/
