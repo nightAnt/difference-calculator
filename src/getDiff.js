4794045dbfba11dba4c83d98ef261b1e34cd3b86
nightAnt <nikolayantoshkin2000@gmail.com>
2023-02-19 21:36:51 +0300

    rename from index.js to getDiff.js

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,26 +1,27 @@
-import getTree from 'getTree.js';
+import _ from 'lodash';
 
-const stringify = (data) => { 
+export function genDiff(data) { 
     let [replacer, spacesCount] =  [' ', 2];
   
     const iter = (innerData, depth) => { 
-      const values = Object.values(innerData); 
-  
+      
       if (!_.isObject(innerData)) { 
         return `${innerData}`; 
       };
-      
+
+      const values = Object.values(innerData); 
+
       const result = values.map( value => { 
         const startIndent = replacer.repeat(depth * spacesCount); 
         
         if (value.status === 'changed') {
-          return `${startIndent}- ${value.key}: ${iter(value.valueBefore, depth + 1)}\n${startIndent}+ ${value.key}: ${iter(value.valueAfter, depth + 1)}`;
+          return `${startIndent}- ${value.key}: ${iter(value.valueBefore, depth + 2)}\n${startIndent}+ ${value.key}: ${iter(value.valueAfter, depth + 1)}`;
         } else if (value.status === 'added') {
-          return `${startIndent}+ ${value.key}: ${iter(value.value, depth + 1)}`; 
+          return `${startIndent}+ ${value.key}: ${iter(value.value, depth + 2)}`; 
         } else if (value.status === 'deleted') {
-          return `${startIndent}- ${value.key}: ${iter(value.value, depth + 1)}`; 
+          return `${startIndent}- ${value.key}: ${iter(value.value, depth + 2)}`; 
         };
-        return `${startIndent}  ${value.key}: ${iter(value.value, depth + 1)}`; 
+        return `${startIndent}  ${value.key}: ${iter(value.value, depth + 2)}`; 
       }); 
   
       const endIndent = replacer.repeat((depth - 1) * spacesCount); 
@@ -29,6 +30,3 @@ const stringify = (data) => {
     }; 
     return iter(data, 1); 
   }; 
-
-  export default stringify;
-  
