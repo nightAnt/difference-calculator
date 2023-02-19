121cb44258e1e83f979dede57ce102d09208e1ef
nightAnt <nikolayantoshkin2000@gmail.com>
2023-02-19 11:09:27 +0300

    update

--- gendiff.js
+++ gendiff.js
@@ -12,34 +12,35 @@ program
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action( (filePath1, filePath2) => {  
-    let ob1 = readFileSync(resolve(filePath1));
-    let ob2 = readFileSync(resolve(filePath2)); 
-    let ress = [];
-    console.log(typeof(ob1));
-    Object.entries(ob1).forEach(([key, value]) => {
-      console.log(key, value);
-    });
-    for (let [key, value] of Object.entries(ob1)) {
-      if (key in ob2) {
-        if (value === ob2[key]) {
-          ress.push([' ', key, value]);
+    let obj1 = JSON.parse(readFileSync(resolve(filePath1)));
+    let obj2 = JSON.parse(readFileSync(resolve(filePath2))); 
+
+    let allEntires = [];
+
+    for (let [key, value] of Object.entries(obj1)) {
+      if (key in obj2) {
+        if (value === obj2[key]) {
+          allEntires.push([' ', key, value]);
         } else {
-          ress.push(['-', key, value]); 
-          ress.push(['+', key, ob2[key]]);
+          allEntires.push(['-', key, value]); 
+          allEntires.push(['+', key, obj2[key]]);
         };
-      } else ress.push(['-', key, value]); 
+      } else allEntires.push(['-', key, value]); 
     };
-    for (let [key, value] of Object.entries(ob2)) {
-      if (!(key in ob1)) {
-        ress.push(['+', key, value]);
+
+    for (let [key, value] of Object.entries(obj2)) {
+      if (!(key in obj1)) {
+        allEntires.push(['+', key, value]);
       };
     };
-    let srt = _.sortBy(ress, (el) => el[1] );
+
+    let sortEntires = _.sortBy(allEntires, (el) => el[1] );
     let result = [];
 
-    srt.forEach(element => {
-      result.push(`${element[0]} ${element[1]}: ${element[2]}`);
+    sortEntires.forEach(element => {
+      result.push(`  ${element[0]} ${element[1]}: ${element[2]}`);
     });
+    
     const out = ['{', ...result, '}'].join('\n');
     console.log(out);
 
@@ -49,10 +50,10 @@ program.parse();
 /*
 let [file1, file2] = program.args;
 let filePath1 = resolve(file1);
-let ob1 = readFileSync(filePath1);
-let ob2 = readFileSync('file2');
+let obj1 = readFileSync(filePath1);
+let obj2 = readFileSync('file2');
 
-console.log(JSON.parse(ob1));
+console.log(JSON.parse(obj1));
 
 
 const stringify = (data, replacer = ' ', spacesCount = 1) => { 
@@ -73,14 +74,14 @@ const stringify = (data, replacer = ' ', spacesCount = 1) => {
 }; 
 
 
-let ress = [];
-for (let [key, value] in Object.entries(ob1)) {
-  if (key in ob2) {
-    if (value === ob2[key]) {
-      ress.push(['', key, value]);
+let allEntires = [];
+for (let [key, value] in Object.entries(obj1)) {
+  if (key in obj2) {
+    if (value === obj2[key]) {
+      allEntires.push(['', key, value]);
     } else {
-      ress.push(['-', key, value]); 
-      ress.push(['+', key, value]);
+      allEntires.push(['-', key, value]); 
+      allEntires.push(['+', key, value]);
     };
   };
 };
