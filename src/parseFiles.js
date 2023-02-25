f56723087c649012f2f3f830d70609c46914a699
nightAnt <nikolayantoshkin2000@gmail.com>
2023-02-25 12:22:16 +0300

    add parse yml files

--- src/parseFiles.js
+++ src/parseFiles.js
@@ -1,26 +1,21 @@
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
+import { load } from 'js-yaml';
 
 function getParsedObjects(filePath1, filePath2) {
-  const currentPath = process.cwd();
   let obj1 = {};
-  let obj2 = {};
-
-  if (filePath1.indexOf(currentPath)) {
-    obj1 = JSON.parse(readFileSync(resolve(filePath1)));
-  } else {
-    obj1 = JSON.parse(readFileSync(resolve(currentPath, filePath1)));
-  }
-
-  if (filePath2.indexOf(currentPath)) {
-    obj2 = JSON.parse(readFileSync(resolve(filePath2)));
-  } else {
-    obj2 = JSON.parse(readFileSync(resolve(currentPath, filePath2)));
-  }
-  /*
+  let obj2 = {}; 
+  
+  if (filePath1.match(/.json$/) && filePath2.match(/.json$/)) {
     obj1 = JSON.parse(readFileSync(resolve(filePath1)));
     obj2 = JSON.parse(readFileSync(resolve(filePath2)));
-    */
+  };
+  
+  if ((filePath1.match(/.yml$|.yaml$/) && filePath2.match(/.yml$|.yaml$/))) {
+    obj1 = load(readFileSync(resolve(filePath1)));
+    obj2 = load(readFileSync(resolve(filePath2)));
+  };
+
   return [obj1, obj2];
 }
 export default getParsedObjects;
