7db5df7fc355b8dacf49062be73b07526b63ada1
nightAnt <nikolayantoshkin2000@gmail.com>
2023-02-25 12:31:07 +0300

    fix linter's mistakes

--- src/parseFiles.js
+++ src/parseFiles.js
@@ -4,17 +4,17 @@ import { load } from 'js-yaml';
 
 function getParsedObjects(filePath1, filePath2) {
   let obj1 = {};
-  let obj2 = {}; 
-  
+  let obj2 = {};
+
   if (filePath1.match(/.json$/) && filePath2.match(/.json$/)) {
     obj1 = JSON.parse(readFileSync(resolve(filePath1)));
     obj2 = JSON.parse(readFileSync(resolve(filePath2)));
-  };
-  
+  }
+
   if ((filePath1.match(/.yml$|.yaml$/) && filePath2.match(/.yml$|.yaml$/))) {
     obj1 = load(readFileSync(resolve(filePath1)));
     obj2 = load(readFileSync(resolve(filePath2)));
-  };
+  }
 
   return [obj1, obj2];
 }
